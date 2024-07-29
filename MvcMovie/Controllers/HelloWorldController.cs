bfa6fb2d0e52fb4cf3f53aefa9f2ef6e31dfbff0
Shanti <prayashincbca2021@gmail.com>
2024-07-29 07:58:58 +0545

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    readonly string name = "Manish";
+   // get : /HelloWorld/ 
+   public IActionResult Index()
+    {
+        return View();
+    }
+    // GET : /HelloWorld/Welcome/ 
+    public string Welcome(string name, int numTimes = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is:{numTimes}"); 
+    }
+}
